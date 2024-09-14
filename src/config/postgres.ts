b4d2f3b7f06688eb0b127525f6704c86b53985af
jagotranaveen <jagotra.naveen@gmail.com>
2024-09-14 23:46:35 +0530

    Update Db config

--- src/config/postgres.ts
+++ src/config/postgres.ts
@@ -3,10 +3,10 @@ import  dotenv from 'dotenv';
 dotenv.config();
 
 const sequelize = new Sequelize({
-  username: "hspadmin",
-  password: "Aug@2024",
-  database: "gxmuserdb",
-  host: "gxmdevdb.postgres.database.azure.com",
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
   dialect: 'postgres',
   dialectOptions: {
     ssl: {
