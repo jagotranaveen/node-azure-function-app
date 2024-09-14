f503775edb42ed3859df1ebcc96b6d69d39cc9ea
jagotranaveen <jagotra.naveen@gmail.com>
2024-09-14 17:26:12 +0530

    ListCompany Servie updated

--- listcompany/index.ts
+++ listcompany/index.ts
@@ -1,16 +1,40 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+const { Client } = require('pg');
+
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully for listcompany - modified response - api.";
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
-
+    const client = new Client({
+        user: "hspadmin",
+        host: "gxmdevdb.postgres.database.azure.com",
+        database: "gxmuserdb",
+        password:"Aug@2024",
+        port: "5432",
+        ssl: {
+                rejectUnauthorized: false // For testing; in production, use the correct CA certificate
+            }
+       
+    });
+    context.log('Before Calling Query.');
+    try {
+        await client.connect();
+        const query = 'SELECT * FROM test_companies';
+        context.log.info(`After Query Execution `);
+        const result = await client.query(query);
+        context.log.info(`After result  mapping `);
+        context.res = {
+            status: 200,
+            body: result.rows
+        };
+    } catch (err) {
+        context.res = {
+            status: 500,
+            body: `Error fetching Comapny::: ${err.message}`
+        };
+    } finally {
+        await client.end();
+    }
+    
 };
 
 export default httpTrigger;
